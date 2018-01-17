5c3e99dff42fef96b79b8f4243d1f0c1e6012253
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-17 14:54:19 -0500

    Update server port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
@@ -17,5 +19,5 @@ app.get('/blog/:id', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('Server running on localhost:3000...')
+  console.log('Server running on Port '+port)
 });
